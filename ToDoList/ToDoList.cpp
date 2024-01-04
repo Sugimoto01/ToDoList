a798065066f98f62df2c3c7a0da5c2c383d1ddf5
Sugimoto <lamkiewicz.kamil@gmail.com>
2024-01-04 21:18:46 +0100

    Working with files
    Add functions to add notes, show them

--- ToDoList/ToDoList.cpp
+++ ToDoList/ToDoList.cpp
@@ -1,5 +1,9 @@
 ﻿#include <iostream>
 #include <fstream>
+#include <string>
+
+
+
 
 class Task {
 
@@ -11,13 +15,24 @@ class Task {
 
 
 public:Task() {
-    std::ofstream MyFile("Tasks.txt");
+    
 }
 
 
 
 public: void AddTask() {
+    std::ofstream MyFile("Tasks.txt");
+    std::string userInput;
 
+
+    if (MyFile.is_open()) {
+        MyFile << userInput << std::endl;
+        MyFile.close();
+    }
+    else std::cerr << "Error opening file!" << std::endl;
+
+
+    MyFile.close();
     }
 
 public:void DelTask() {
@@ -29,30 +44,43 @@ public:void SetTaskDone() {
 
 public:void ShowTasks() {
 
+    std::ifstream MyFile("Tasks.txt");
+
+    if (MyFile.is_open()) {
+        std::string line;
+        while (std::getline(MyFile, line)) {
+            std::cout << line << std::endl;
+        }
+    }
+    else std::cerr << "Error opening file!" << std::endl;
+
+    MyFile.close();
 }
-
-
-
 };
 
 
 void ShowMenu() {
-        std::cout << "1.Dodaj Zadanie\n";
-        std::cout << "2.Usuń Zadanie\n";
-        std::cout << "3.Zobacz swoje zadania Zadanie\n";
-        std::cout << "4.Oznacz zadanie jako wykonane\n";
+        std::cout << "1.Dodaj Zadanie"<<std::endl;
+        std::cout << "2.Usuń Zadanie" << std::endl;
+        std::cout << "3.Zobacz swoje zadania Zadanie" << std::endl;
+        std::cout << "4.Oznacz zadanie jako wykonane" << std::endl;
         std::cout << "5.Wyjdź\n";
 }
 
 
 int main()
 {
+
+    
     Task myTask;
     ShowMenu();
 
+   
 
 
-  
+
+   
+    return 0;
 }
 
 
@@ -60,4 +88,24 @@ int main()
 Napisz rozbudowaną aplikację do zarządzania listą zadań (To-Do List). Aplikacja powinna umożliwiać użytkownikowi dodawanie,
 usuwanie i oznaczanie zadań jako ukończone. Wykorzystaj klasy i struktury do reprezentowania zadań 
 oraz zastosuj operacje na plikach do zapisywania i wczytywania listy zadań.
+*/
+
+
+/*
+Uzytkownik włącza program i wybiera co chcę zrobić ze swoją listą
+
+Na początku program musi sprawdzić czy plik z listą isnieje jeżeli tak to go wczytać 
+a gdy nie ma takiego pliku to go stworzyć
+
+
+funkcja dodaj zadanie będzie dodawać tekst do pliku
+
+funkcja usuń zadanie będzie musiała usunąć całą linie bo na każde zadanie przysługuje 1 linia
+
+funkcja zobacz zadanie wyświetli zawartość pliku
+
+funkcja oznacz zadanie jako wykonane doda na końcu tekstu w lini ptazska
+
+
+
 */
