d9d2664f31c7755ea82c19bd58363e59115d9a59
Sugimoto <lamkiewicz.kamil@gmail.com>
2024-01-07 12:54:30 +0100

    AddCode
    Add rest functionalites to the program

--- ToDoList/ToDoList.cpp
+++ ToDoList/ToDoList.cpp
@@ -1,9 +1,12 @@
 ﻿#include <iostream>
 #include <fstream>
 #include <string>
+#include <chrono>
+#include <vector>
 
 
 
+const auto TimeStamp = std::chrono::system_clock::now(); // create time stamp of actual time from system
 
 class Task {
 
@@ -21,12 +24,16 @@ public:Task() {
 
 
 public: void AddTask() {
-    std::ofstream MyFile("Tasks.txt");
-    std::string userInput;
+    std::ofstream MyFile("Tasks.txt", std::ios::app);
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+    std::cout << "Wpisz tekst: ";
+    std::getline(std::cin, Description);
+
 
 
     if (MyFile.is_open()) {
-        MyFile << userInput << std::endl;
+        MyFile << Description << " " << std::chrono::duration_cast<std::chrono::seconds>(TimeStamp.time_since_epoch()).count() << std::endl;
         MyFile.close();
     }
     else std::cerr << "Error opening file!" << std::endl;
@@ -36,6 +43,32 @@ public: void AddTask() {
     }
 
 public:void DelTask() {
+
+    std::ifstream Myfile("Tasks.txt");
+    std::ofstream outputFile("Modified.txt");
+
+    if (Myfile.is_open() && outputFile.is_open()) {
+        std::vector<std::string> linesToDelete = { "Line to delete 1", "Line to delete 2" };
+
+        std::string line;
+        while (getline(Myfile, line)) {
+            // Check if the current line should be deleted
+            if (std::find(linesToDelete.begin(), linesToDelete.end(), line) == linesToDelete.end()) {
+                // If not, write the line to the new file
+                outputFile << line << std::endl;
+            }
+        }
+
+        Myfile.close();
+        outputFile.close();
+
+        // Optionally, you can replace the original file with the modified one
+        std::remove("Task.txt"); // Remove the original file
+        std::rename("modified.txt", "Task.txt"); // Rename the modified file
+    }
+    else {
+        std::cerr << "Error opening files!" << std::endl;
+    }
 
 }
 public:void SetTaskDone() {
@@ -70,13 +103,24 @@ void ShowMenu() {
 
 int main()
 {
-
+    int choose;
     
     Task myTask;
     ShowMenu();
 
-   
+    std::cout << "Co chcesz zrobić:";
+    std::cin >> choose;
 
+    switch (choose)
+    {
+    case 1: myTask.AddTask(); break;
+    case 2: myTask.DelTask(); break;
+    case 3: myTask.ShowTasks(); break;
+    case 4: myTask.SetTaskDone(); break;
+    case 5: return 0; break;
+    default:std::cout << "Nieprawidlowy wybor."; return 0;
+        break;
+    }
 
 
    
@@ -106,6 +150,8 @@ funkcja zobacz zadanie wyświetli zawartość pliku
 
 funkcja oznacz zadanie jako wykonane doda na końcu tekstu w lini ptazska
 
+
+problem po ponownym uruchomieniu programu nadpisuję on to co już było poprzednio
 
 
 */
